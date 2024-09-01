21e3fa8dddc7ba8a3323001957115fbf0d02afaf
ThienLe2112 <khongchapga@gmail.com>
2024-09-01 09:21:28 +0800

    Update MS-G3D

--- model/msg3d.py
+++ model/msg3d.py
@@ -127,44 +127,10 @@ class Model(nn.Module):
 
         self.data_bn = nn.BatchNorm1d(num_person * in_channels * num_point)
 
-        #Original Code Begin
-        # # channels
-        # c1 = 96
-        # c2 = c1 * 2     # 192
-        # c3 = c2 * 2     # 384
-
-        # # r=3 STGC blocks
-        # self.gcn3d1 = MultiWindow_MS_G3D(3, c1, A_binary, num_g3d_scales, window_stride=1)
-        # self.sgcn1 = nn.Sequential(
-        #     MS_GCN(num_gcn_scales, 3, c1, A_binary, disentangled_agg=True),
-        #     MS_TCN(c1, c1),
-        #     MS_TCN(c1, c1))
-        # self.sgcn1[-1].act = nn.Identity()
-        # self.tcn1 = MS_TCN(c1, c1)
-
-        # self.gcn3d2 = MultiWindow_MS_G3D(c1, c2, A_binary, num_g3d_scales, window_stride=2)
-        # self.sgcn2 = nn.Sequential(
-        #     MS_GCN(num_gcn_scales, c1, c1, A_binary, disentangled_agg=True),
-        #     MS_TCN(c1, c2, stride=2),
-        #     MS_TCN(c2, c2))
-        # self.sgcn2[-1].act = nn.Identity()
-        # self.tcn2 = MS_TCN(c2, c2)
-
-        # self.gcn3d3 = MultiWindow_MS_G3D(c2, c3, A_binary, num_g3d_scales, window_stride=2)
-        # self.sgcn3 = nn.Sequential(
-        #     MS_GCN(num_gcn_scales, c2, c2, A_binary, disentangled_agg=True),
-        #     MS_TCN(c2, c3, stride=2),
-        #     MS_TCN(c3, c3))
-        # self.sgcn3[-1].act = nn.Identity()
-        # self.tcn3 = MS_TCN(c3, c3)
-
-        #Original Code End 
-
-        #Edit Code Begin 14
         # channels
         c1 = 96
-        c2 = c1 * 2    # 192
-        c3 = c2 *2   # 384
+        c2 = c1 * 2     # 192
+        c3 = c2 * 2     # 384
 
         # r=3 STGC blocks
         self.gcn3d1 = MultiWindow_MS_G3D(3, c1, A_binary, num_g3d_scales, window_stride=1)
@@ -190,45 +156,40 @@ class Model(nn.Module):
             MS_TCN(c3, c3))
         self.sgcn3[-1].act = nn.Identity()
         self.tcn3 = MS_TCN(c3, c3)
-        ##Edit Code End 14
+
         self.fc = nn.Linear(c3, num_class)
 
     def forward(self, x):
         N, C, T, V, M = x.size()
         ##Edit Code Begin 3
         xx= torch.clone(x)
+        # print('x.shape: ',x.shape)
         ##Edit Code End 3
 
         x = x.permute(0, 4, 3, 1, 2).contiguous().view(N, M * V * C, T)
         x = self.data_bn(x)
-        # xxx_1=torch.clone(x)
         x = x.view(N * M, V, C, T).permute(0,2,3,1).contiguous()
         
         ##Edit Code Begin 13
-        # xxx_1=torch.clone(x)
-        # xx = xxx_1.permute(0,2,1,3).contiguous().view(N * M * T, C, V)
-        # xx = xxx_1
-
-
+        
         # xx= torch.clone(x)
-
-        # print(xxx.shape)
-        # print(xx.shape)
-        # print(xxx[300,2,:])
-        # print(xx[1,2,0,:])
+        
+        
+        # xx=torch.clone(x)
+        # xx = xx.permute(0,2,1,3).contiguous().view(N * M * T, C, V)
         ##Edit Code End 13
         
 
 
         # Apply activation to the sum of the pathways
         ##Edit Code Begin 4
-        x = F.relu(self.sgcn1(x) + self.gcn3d1([x,xx]), inplace=True)
+        x = F.relu(self.sgcn1([x,xx]) + self.gcn3d1([x,xx]), inplace=True)
         x = self.tcn1(x)
 
-        x = F.relu(self.sgcn2(x) + self.gcn3d2([x,xx]), inplace=True)
+        x = F.relu(self.sgcn2([x,xx]) + self.gcn3d2([x,xx]), inplace=True)
         x = self.tcn2(x)
 
-        x = F.relu(self.sgcn3(x) + self.gcn3d3([x,xx]), inplace=True)
+        x = F.relu(self.sgcn3([x,xx]) + self.gcn3d3([x,xx]), inplace=True)
         x = self.tcn3(x)
 
         ##Edit Code End 4
